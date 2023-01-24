36aa21a2d1e3549f5c537b20e1683be0fc1a6a78
alejo117lp <60120226+alejo117lp@users.noreply.github.com>
2023-01-24 14:59:04 -0500

    cj_esfera_end

--- Assets/Scripts/CjStates.cs
+++ Assets/Scripts/CjStates.cs
@@ -9,34 +9,35 @@ public class CjStates : MonoBehaviour
     }
 
     [SerializeField] private State currentState;
+    [SerializeField] private Material mMaterial;
+    [SerializeField] Color floatingColor;
+    [SerializeField] Color normalColor;
     Rigidbody mrigidbody;
 
     private void Awake() {
-
         mrigidbody = GetComponent<Rigidbody>();
-
     }
     void Update() {
 
         if (currentState == State.Bounce) {
 
             if (Input.GetKeyDown(KeyCode.Space)) {
-
                 currentState = State.Float;
-
                 mrigidbody.isKinematic = true;
-
+                mMaterial.SetColor("_BaseColor", floatingColor);
             }
 
             if (Input.GetKeyDown(KeyCode.E)) {
                 currentState = State.Expand;
-                transform.localScale = Vector3.one * 5;
+                transform.localScale = Vector3.one * 5; //Expandir escalando en todos los ejes
             }
         }
+
         else {
             if (currentState == State.Float && Input.GetKeyUp(KeyCode.Space)) {
                 currentState = State.Bounce;
                 mrigidbody.isKinematic = false;
+                mMaterial.SetColor("_BaseColor", normalColor);
             }
 
             if (currentState == State.Expand && Input.GetKeyUp(KeyCode.E)) {
